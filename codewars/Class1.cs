ed7825e86af51cff08a4d8c0f98380a97e00c565
Darkn35s <scoolboy1928@gmail.com>
2022-06-29 16:03:36 +0300

    Добавьте файлы проекта.

---
+++ codewars/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codewars
+{
+    public class Number
+    {
+        public int DigitalRoot(long n)
+        {
+            int result = 0;
+            while (n != 0)
+            {
+                result += (int)(n % 10);
+                n=n / 10;
+            }
+            if (result >= 10) return DigitalRoot(result);
+            else return result;
+        }
+        public int DigitalRoot2(long n)
+        {
+            return (int)(1 + (n - 1) % 9);
+        }
+    }
+
+    public class Kata
+    {
+        public static bool IsIsogram(string str)
+        {
+            List<char> lst=new List<char>();
+            foreach(char ch in str)
+            {
+                if (lst.Contains(ch)) return false;
+                lst.Add(ch);
+            }
+            return true;
+        }
+
+        public static string ToCamelCase(string str)
+        {
+            return str;
+        }
+    }
+}
+
+
+}
---
+++ codewars/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using codewars;
+
+public class Program {
+    public static void Main()
+    {
+        
+        Rasim a = new Rasim();
+        var startTime = System.Diagnostics.Stopwatch.StartNew();
+        a.hren(100000);
+        startTime.Stop();
+        var resultTime = startTime.Elapsed;
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}",
+        resultTime.Hours,
+        resultTime.Minutes,
+        resultTime.Seconds,
+        resultTime.Milliseconds);
+        Console.WriteLine(elapsedTime);
+    }
+
+}
+
